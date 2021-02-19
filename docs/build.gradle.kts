9304201b4f37a1f0ad7a8f08d08098b3e0e6cdd7
Kim A. Betti <kim.betti@gmail.com>
2021-02-19 10:22:23 +0100

    Build docs to /site folder

--- docs/build.gradle.kts
+++ docs/build.gradle.kts
@@ -14,6 +14,7 @@ repositories {
 
 tasks {
     "asciidoctor"(AsciidoctorTask::class) {
+        setOutputDir(file("../site"))
         outputOptions {
             if (project.hasProperty("pdf")) {
                 backends("pdf")
