25501b8d488acf1419bf6e291c77b21ceff24efb
Kim A. Betti <kim.betti@gmail.com>
2021-03-23 14:35:12 +0100

    Gradle - Dropped owasp plugin (Dependabot does this and is automated)

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     id("org.jetbrains.kotlin.jvm") version "1.4.31"
     id("com.adarshr.test-logger") version "2.1.1"
     id("com.google.protobuf") version "0.8.15"
-    id("org.owasp.dependencycheck") version "6.1.1" apply false
     id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
 }
 
