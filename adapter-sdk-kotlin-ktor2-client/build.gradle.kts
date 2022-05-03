b959b71cc86934b065776fc7074b42bacf7fb859
Kim A. Betti <kim.betti@origo.oslo.kommune.no>
2022-05-03 10:41:49 +0200

    Client for fetching public keys (#94)

---
+++ adapter-sdk-kotlin-ktor2-client/build.gradle.kts
@@ -0,0 +1,55 @@
+import com.adarshr.gradle.testlogger.TestLoggerExtension
+import com.adarshr.gradle.testlogger.TestLoggerPlugin
+
+plugins {
+    `java-library`
+    `maven-publish`
+    idea
+
+    id("org.jetbrains.kotlin.jvm")
+    id("com.adarshr.test-logger")
+    id("java")
+}
+
+description = "Ktor client for fetching public keys"
+val ktorVersion = "2.0.1"
+
+
+dependencies {
+    implementation("org.slf4j:slf4j-api:1.7.36")
+    api("com.nimbusds:nimbus-jose-jwt:9.22")
+
+    implementation("io.ktor:ktor-client-json:$ktorVersion")
+    implementation("io.ktor:ktor-client-core-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+
+    testImplementation("org.slf4j:slf4j-simple:1.7.36")
+    testImplementation("org.assertj:assertj-core:3.22.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+}
+
+plugins.withType<TestLoggerPlugin> {
+    configure<TestLoggerExtension> {
+        theme = com.adarshr.gradle.testlogger.theme.ThemeType.MOCHA_PARALLEL
+        slowThreshold = 5000
+        showStackTraces = true
+        showCauses = true
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+    reports {
+        html.required.set(false)
+        junitXml.required.set(true)
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+}
