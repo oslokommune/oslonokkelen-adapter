73d47e33065522f6b8913f5dfc5c9876a1bea1cb
Lily E.S <51922940+lily-es@users.noreply.github.com>
2024-11-14 09:55:26 +0100

    Ktor 3.0.1 (#314)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,4 +3,4 @@ rootProject.name = "oslonokkelen-adapter"
 include(":adapter-docs")
 include(":adapter-protobuf-java")
 include(":adapter-sdk-kotlin")
-include(":adapter-sdk-kotlin-ktor2-client")
+include(":adapter-sdk-kotlin-ktor3-client")
